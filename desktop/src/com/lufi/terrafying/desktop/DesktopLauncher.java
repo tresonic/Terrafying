7843a9ec377015828ec60effcb59b02b112b260b
Tresonic <tresonic@mail.de>
2021-05-11 15:35:07 +0200

    run only server via command line argument

--- desktop/src/com/lufi/terrafying/desktop/DesktopLauncher.java
+++ desktop/src/com/lufi/terrafying/desktop/DesktopLauncher.java
@@ -3,15 +3,20 @@ package com.lufi.terrafying.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.lufi.terrafying.Terrafying;
+import com.lufi.terrafying.net.TerrafyingServer;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1280;
-		config.height = 720;
-		config.title = "Terrafying";
-		// add icon in the future!!!
-		// config.addIcon(path, fileType);
-		new LwjglApplication(new Terrafying(), config);
+		if(arg.length > 0 && arg[0].contentEquals("server")) {
+			TerrafyingServer.the().start();
+		} else {
+			LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+			config.width = 1280;
+			config.height = 720;
+			config.title = "Terrafying";
+			// add icon in the future!!!
+			// config.addIcon(path, fileType);
+			new LwjglApplication(new Terrafying(), config);
+		}
 	} 
 }
